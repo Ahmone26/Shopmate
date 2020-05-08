f2fdd3e1503b154040a9cb633dd76e6d61b43531
Ahmed Džanan <ahmed.dzanan@edu.fit.ba>
2020-05-08 14:37:32 +0200

    Home page update pt.4

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
         return (
             <div className="container">
                  <div className="home">   
-                     <img src="/images/home1.png" alt="" className="heroSlika"/>
+                     <img src="/images/Background.png" alt="" className="heroSlika"/>
                     <button className="btn">View All</button>
                  </div>
                  <div className="container2">
@@ -56,6 +56,42 @@ class Home extends Component {
                              </div>  
                              
                         </form> 
+                 </div>
+                 <div className="fifth">
+                     <div className="fifth-1">
+                         <table className="table">
+                             <thead className="thead">
+                                 <tr>
+                                     <th>QUESTIONS?</th>
+                                     <th>WHAT'S IN STORE</th>
+                                     <th>FOLLOW US</th>
+                                     <td>©2016 shopmate Ltd</td>
+                                 </tr>
+                             </thead>
+                             <tbody>
+                                 <tr>
+                                    <td>Help</td>
+                                    <td>Women</td>
+                                    <td>Facebook</td>
+                                 </tr>
+                                 <tr>
+                                    <td>Track Order</td>
+                                    <td>Men</td>
+                                    <td>Twitter</td>
+                                 </tr>
+                                 <tr>
+                                    <td>Returns</td>
+                                    <td>Product A-Z</td>
+                                    <td>YouTube</td>
+                                 </tr>
+                                 <tr>
+                                    <td></td>
+                                    <td>Buy Gift Voucher</td>
+                                 </tr>
+                             </tbody>
+                         </table>
+                     </div>
+
                  </div>
             </div>
 
